864e50fce16e1c8e7c032048dc24768d40ced40b
gongbaodd <gongbaodd@gmail.com>
2015-03-18 14:08:26 +0800

    todo add

--- todoMVC/js/todos.ts
+++ todoMVC/js/todos.ts
@@ -67,9 +67,205 @@ declare var _:{
 declare var Store:any;
 
 class Todo extends Backbone.Model {
+    defaults() {
+        return {
+            content:"empty todo ...",
+            done:false
+        }
+    }
 
+    initialize() {
+        if(!this.get("content")) {
+            this.set({"content":this.defaults().content});
+        }
+    }
+
+    toggle() {
+        this.save({done:!this.get("done")});
+    }
+
+    clear() {
+        this.destroy();
+    }
+}
+
+class TodoList extends Backbone.Collection<Todo> {
+    model = Todo;
+    localStorage = new Store("todos-backbone");
+
+    done() {
+        return this.filter(todo => todo.get('done'));
+    }
+
+    remaining() {
+        return this.without.apply(this.this.done());
+    }
+
+    nextOrder() {
+        if (!this.length) return 1;
+        return this.last().get('order') + 1;
+    }
+
+    comparator(todo: Todo) {
+        return todo.get('order');
+    }
+}
+
+var Todos = new TodoList();
+
+class TodoView extends Backbone.View {
+    template: (data: any) => string;
+
+    model: Todo;
+    input: JQuery;
+
+    constructor (options?) {
+        this.tagName = "li";
+
+        this.events = {
+                    "click .check":"toggleDone",
+                    "dblclick label.todo-content":"edit",
+                    "click span.todo-destory":"clear",
+                    "keypress .todo-input":"updateOnEnter",
+                    "blur .todo-input":"close"
+                 };
+
+        super(options);
+
+        this.template = _.template($('#item-template').html());
+        _.bindAll(this,'render','close','remove');
+        this.model.bind('change',this.render);
+        this.model.bind('destory',this.remove);
+    }
+
+    render() {
+        this.$el.html(this.template(this.model.toJSON()));
+        this.input = this.$('.todo-input');
+        return this;
+    }
+
+    toggleDone() {
+        this.model.toggle();
+    }
+
+    edit() {
+        this.$el.addClass('editing');
+        this.input.focus();
+    }
+
+    close() {
+        this.model.save({content:this.input.val()});
+        this.$el.removeClass('editing');
+    }
+
+    updateOnEnter(e) {
+        if (e.keyCode == 13) close();
+    }
+
+    clear() {
+        this.model.clear();
+    }
+}
+
+class AppView extends Backbone.View {
+    events = {
+        "keypress #new-todo":"createOnEnter",
+        "keyup #new-todo":"showTooltip",
+        "click .todo-clear a":"clearCompleted",
+        "click .mark-all-done":"toggleAllComplete"
+    };
+
+    input: JQuery;
+    allCheckbox: HTMLInputElement;
+    statsTemplate: (params:any) => string;
+
+    constructor () {
+        super();
+
+        this.setElement($("#todoapp"),true);
+        _.bindAll(this, 'addOne', 'addAll', 'render', 'toggleAllComplete');
+
+        this.input = this.$("#new-todo");
+        this.allCheckbox = this.$(".mark-all-done")[0];
+        this.statsTemplate = _.template($('#stats-template').html());
+
+        Todos.bind('add', this.addOne);
+        Todos.bind('reset', this.addAll);
+        Todos.bind('all', this.render);
+
+        Todos.fetch();
+    }
+
+
+    render() {
+        var done = Todos.done().length;
+        var remaining = Todos.remaining().length;
+
+        this.$('#todo-stats').html(this.statsTemplate({
+            total: Todos.length,
+            done: done,
+            remaining: remaining
+        }));
+
+        this.allCheckbox.checked = !remaining;
+    }
+
+    // Add a single todo item to the list by creating a view for it, and
+    // appending its element to the `<ul>`.
+    addOne(todo) {
+        var view = new TodoView({ model: todo });
+        this.$("#todo-list").append(view.render().el);
+    }
+
+    // Add all items in the **Todos** collection at once.
+    addAll() {
+        Todos.each(this.addOne);
+    }
+
+    // Generate the attributes for a new Todo item.
+    newAttributes() {
+        return {
+            content: this.input.val(),
+            order: Todos.nextOrder(),
+            done: false
+        };
+    }
+
+    // If you hit return in the main input field, create new **Todo** model,
+    // persisting it to *localStorage*.
+    createOnEnter(e) {
+        if (e.keyCode != 13) return;
+        Todos.create(this.newAttributes());
+        this.input.val('');
+    }
+
+    // Clear all done todo items, destroying their models.
+    clearCompleted() {
+        _.each(Todos.done(), todo => todo.clear());
+        return false;
+    }
+
+    tooltipTimeout: number = null;
+    // Lazily show the tooltip that tells you to press `enter` to save
+    // a new todo item, after one second.
+    showTooltip(e) {
+        var tooltip = $(".ui-tooltip-top");
+        var val = this.input.val();
+        tooltip.fadeOut();
+        if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
+        if (val == '' || val == this.input.attr('placeholder')) return;
+        this.tooltipTimeout = _.delay(() => tooltip.show().fadeIn(), 1000);
+    }
+
+    toggleAllComplete() {
+        var done = this.allCheckbox.checked;
+        Todos.each(todo => todo.save({ 'done': done }));
+    }
 }
 
 
-
+$(() => {
+    // Finally, we kick things off by creating the **App**.
+    new AppView();
+});
 
